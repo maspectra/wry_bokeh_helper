c6df113b860fd24fdc8c18c400a80fd285fbd1a5
johnnyxcy <johnny.xcy1997@outlook.com>
2024-10-25 23:40:56 +0800

    feat: support dpi

--- wry_bokeh_helper/_wry_bokeh_helper.pyi
+++ wry_bokeh_helper/_wry_bokeh_helper.pyi
@@ -2,6 +2,8 @@ from typing import Literal
 
 ResourceType = Literal["cdn", "local"]
 
-def render_bokeh(json: str, resource: tuple[ResourceType, str] | None = None) -> str:
+def render_bokeh(
+    json_data: str, dpi: int = 300, resource: tuple[ResourceType, str] | None = None
+) -> str:
     """Render Bokeh JSON to a image URL."""
     ...
